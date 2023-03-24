6a6f086971a1c8e36cd300a70478710166e02e46
xada <wahabzada00@gmail.com>
2023-03-24 23:51:48 +0500

    'tweaks on dark mode. now its working great on dark mode as well.'

--- dist/script.js
+++ dist/script.js
@@ -1,4 +1,5 @@
 
+
 /* ----------------- Modal Js ----------------- */
 const button = document.querySelector('.nav__btn a');
 const modalWrapper = document.querySelector('.modal-wrapper');
@@ -17,30 +18,4 @@ closeModalButton.addEventListener('click', function() {
 /* --------------- Modal Js Ends -------------- */
 
 
-/* --------------- Late Payment --------------- */
-// Set the due date for the payment
-const dueDate = new Date('2023-01-01');
 
-// Get the current date
-const currentDate = new Date();
-
-// Calculate the number of days late
-const daysLate = Math.floor((currentDate - dueDate) / (1000 * 60 * 60 * 24));
-
-// Set the penalty rate per day
-const penaltyRate = 0.1;
-
-// Calculate the penalty amount
-const penaltyAmount = penaltyRate * daysLate;
-
-// If the user is late on payment, disable the website gradually
-if (penaltyAmount > 0) {
-  const disableWebsite = setInterval(() => {
-    const currentOpacity = parseFloat(document.body.style.opacity);
-    if (currentOpacity > 0) {
-      document.body.style.opacity = (currentOpacity - 0.1).toString();
-    } else {
-      clearInterval(disableWebsite);
-    }
-  }, 24 * 60 * 60 * 1000);
-}
